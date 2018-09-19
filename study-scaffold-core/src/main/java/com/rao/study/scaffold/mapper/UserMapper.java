721c68298d9b5266a9f6c756499aedb841e60e90
honey.rao <honey.rao@wolaidai.com>
2018-09-19 18:54:26 +0800

    1.添加mybatis依赖 2.添加自动生成mapper和实体的maven插件

---
+++ study-scaffold-core/src/main/java/com/rao/study/scaffold/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.rao.study.scaffold.mapper;
+
+import com.rao.study.scaffold.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ study-scaffold-core/src/main/java/com/rao/study/scaffold/model/User.java
@@ -0,0 +1,79 @@
+package com.rao.study.scaffold.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Date birthday;
+
+    private Date createAt;
+
+    private Date updateAt;
+
+    private Integer age;
+
+    private BigDecimal salary;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+}
